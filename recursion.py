a8b3316e5bcb9d6172bd70b38882e67358b1715b
Kabin <kabinghale1@gmail.com>
2024-02-17 17:57:05 +0545

    practiced recursion

---
+++ recursion.py
@@ -0,0 +1,29 @@
+
+
+def fact(n):
+    if(n < 1):
+        return 1;
+    
+    return n * fact(n-1);
+
+print(fact(5));
+
+def fib(n):
+    if(n < 2):
+        return 1;
+    
+    return fib(n-1) + fib(n-2);
+
+
+print(fib(5));
+
+
+def Toh(n,start_rod,inter,dest_rod):
+    if(n == 0):
+        return;
+    
+    Toh(n-1,start_rod,dest_rod,inter);
+    print("Disk",n,"moved from",start_rod,"to",inter);
+    return Toh(n-1,dest_rod,inter,start_rod);
+
+Toh(4,'A','B','C');
